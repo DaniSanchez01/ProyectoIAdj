b7f746ed5999ae6731dd17c34a651d99849c8e45
IM4441564944nicetomeetyou <davidffff0101@gmail.com>
2023-02-12 19:22:54 +0100

    Creación de velocity matching
    He implementado el steering velocity matching, aunque aún tengo que probarlo para ver si funciona bien.

--- Assets/Semana2/ScriptsAI/Steering/Basic/VelocityMatching.cs
+++ Assets/Semana2/ScriptsAI/Steering/Basic/VelocityMatching.cs
@@ -6,20 +6,32 @@ public class VelocityMatching : SteeringBehaviour
 {
 
     // Declara las variables que necesites para este SteeringBehaviour
-
+    public Agent target;
+    //tiempo hacia el objetivo, recordamos que a valores mas altos aceleraciones/desaceleraciones
+    //mas suaves y a mas bajos mas bruscas
+    public float timetoTarget = 0.1f;
+    
     
     void Start()
     {
-        this.nameSteering = "Pon su nombre";
+        this.nameSteering = "VelocityMatching";
     }
 
 
     public override Steering GetSteering(Agent agent)
     {
+
         Steering steer = new Steering();
-
         // Calcula el steering.
+        steer.linear = target.Velocity - agent.Velocity;
+        steer.linear = steer.linear / timetoTarget;
 
+        if(steer.linear.magnitude > agent.MaxAcceleration)
+        {
+            steer.linear = steer.linear.normalized * agent.MaxAcceleration;
+        }
+
+        steer.angular = 0;
         // Retornamos el resultado final.
         return steer;
     }
