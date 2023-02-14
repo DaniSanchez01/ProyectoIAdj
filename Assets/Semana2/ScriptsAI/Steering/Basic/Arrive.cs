207aac3969a98ce35315eb35902eed9e060d55aa
DaniSanchez01 <danielsnchez01@gmail.com>
2023-02-14 12:48:41 +0100

    Update Arrive.cs

--- Assets/Semana2/ScriptsAI/Steering/Basic/Arrive.cs
+++ Assets/Semana2/ScriptsAI/Steering/Basic/Arrive.cs
@@ -4,37 +4,50 @@ using UnityEngine;
 
 public class Arrive : SteeringBehaviour
 {
+    public Vector3 targetPosition; // El objetivo hacia el que nos estamos acercando
+    public float maxSpeed; // Velocidad máxima que puede tener el agente
+    public float arrivalRadius; // Radio de satisfacción
+    public float timeToTarget = 0.1f; // Tiempo para llegar al objetivo ?
+    public float maxAcceleration; // Aceleración máxima que puede tener el agente
 
-    // Declara las variables que necesites para este SteeringBehaviour
-    public Agent target;
-    public float timeToTarget = 0.2f;
-    
     void Start()
     {
         this.nameSteering = "Arrive";
     }
 
-
     public override Steering GetSteering(Agent agent)
     {
         Steering steer = new Steering();
 
-        // Calcula el steering.
-        Vector3 direction = target.Position - agent.Position;
+        // Calcular la dirección hacia el objetivo
+        Vector3 direction = targetPosition - agent.transform.position; // podríamos usar la Transform del target?
         float distance = direction.magnitude;
 
-        if (distance < target.interiorRadius) {
-            return steer;
-        }
-        if (distance > target.interiorRadius)
+        // Calcular la velocidad dentro del radio de llegada
+        float targetSpeed = maxSpeed;
+        if (distance < arrivalRadius)
         {
-            float agentSpeeed = agent.MaxSpeed; 
+            targetSpeed = maxSpeed * distance / arrivalRadius;
         }
-        else
-        {
 
-        } 
-        // Retornamos el resultado final.
+        // Calcular la velocidad deseada
+        Vector3 targetVelocity = direction;
+        targetVelocity.Normalize();
+        targetVelocity *= targetSpeed;
+
+        steer.linear = targetVelocity - agent.Velocity;
+        steer.linear /= timeToTarget;
+
+        if (steer.linear.magnitude > maxAcceleration)
+        {
+            steer.linear.Normalize();
+            steer.linear *= maxAcceleration;
+        }
+
+        // Devolver el resultado final
+        steer.angular = 0;
         return steer;
     }
 }
+
+
