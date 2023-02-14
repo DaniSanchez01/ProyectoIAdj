423ad4795b7a0a87b60f73dcc5e6827283670a25
DaniSanchez01 <danielsnchez01@gmail.com>
2023-02-14 12:55:14 +0100

    .

--- Assets/Semana2/ScriptsAI/Steering/Basic/Arrive.cs
+++ Assets/Semana2/ScriptsAI/Steering/Basic/Arrive.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Arrive : SteeringBehaviour
 {
-    public Vector3 targetPosition; // El objetivo hacia el que nos estamos acercando
-    public float maxSpeed; // Velocidad máxima que puede tener el agente
-    public float arrivalRadius; // Radio de satisfacción
+
+    public Agent target;
     public float timeToTarget = 0.1f; // Tiempo para llegar al objetivo ?
-    public float maxAcceleration; // Aceleración máxima que puede tener el agente
 
     void Start()
     {
@@ -20,14 +18,14 @@ public class Arrive : SteeringBehaviour
         Steering steer = new Steering();
 
         // Calcular la dirección hacia el objetivo
-        Vector3 direction = targetPosition - agent.transform.position; // podríamos usar la Transform del target?
+        Vector3 direction = target.Position - agent.transform.position; // podríamos usar la Transform del target?
         float distance = direction.magnitude;
 
         // Calcular la velocidad dentro del radio de llegada
-        float targetSpeed = maxSpeed;
-        if (distance < arrivalRadius)
+        float targetSpeed = target.MaxSpeed;
+        if (distance < target.arrivalRadius)
         {
-            targetSpeed = maxSpeed * distance / arrivalRadius;
+            targetSpeed = target.MaxSpeed * distance / target.arrivalRadius;
         }
 
         // Calcular la velocidad deseada
@@ -38,10 +36,10 @@ public class Arrive : SteeringBehaviour
         steer.linear = targetVelocity - agent.Velocity;
         steer.linear /= timeToTarget;
 
-        if (steer.linear.magnitude > maxAcceleration)
+        if (steer.linear.magnitude > target.MaxAcceleration)
         {
             steer.linear.Normalize();
-            steer.linear *= maxAcceleration;
+            steer.linear *= target.MaxAcceleration;
         }
 
         // Devolver el resultado final
