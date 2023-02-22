2d99b1e5ae3e1482b2c368d708588cf38d01434c
IM4441564944nicetomeetyou <davidffff0101@gmail.com>
2023-02-22 15:04:29 +0100

    Añadidos 2 tipos de agentes más
    He añadido 2 tipos de agentes. Uno simboliza un agente más pesado y en su funcion "start()" he modificado algunos de sus parametros fisicos y el otro simboliza un agente que se usara a distancia. Del último también he modificado sus parametros físicos.

---
+++ Assets/Semana2/ScriptsAI/NPC/ArchierAgentNPC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchierAgentNPC : AgentNPC
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Mass = 1; //tiene mas masa
+        this.MaxSpeed = 1.2f; // la maxima velocidad tambien se disminuye
+        this.MaxRotation = 1.2f; //la maxima velocidad angular que puede tener al ser mas pesado tienes menos velocidad angular
+        this.MaxAcceleration = 1.2f; //la maxima aceleracion tambien la he reducido
+        this.MaxAngularAcc = 1.1f; //la maxima aceleracion agular tambien se ha reducido
+        this.MaxForce = 1; //la maxima fuerza
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+
+}
---
+++ Assets/Semana2/ScriptsAI/NPC/TankAgentNPC.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAgentNPC : AgentNPC
+{
+
+
+    /*
+     * Este personaje representaria un personaje pesado y que por tanto tiene mucha masa y poca aceleración aunque se puede seguir moviendo
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Mass = 5; //tiene mas masa
+        this.MaxSpeed = 0.8f; // la maxima velocidad tambien se disminuye
+        this.MaxRotation = 0.8f; //la maxima velocidad angular que puede tener al ser mas pesado tienes menos velocidad angular
+        this.MaxAcceleration = 0.8f; //la maxima aceleracion tambien la he reducido
+        this.MaxAngularAcc = 0.6f; //la maxima aceleracion agular tambien se ha reducido
+        this.MaxForce = 5; //la maxima fuerza
+        
+    }
+
+    // Update is called once per frame
+    public  override void Update()
+    {
+        base.Update();
+    }
+
+    public override void LateUpdate()
+    {
+        base.LateUpdate();
+    }
+}
