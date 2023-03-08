2b28e14170da4123dbcea8a0c15cb46a9fc6c1ab
IM4441564944nicetomeetyou <davidffff0101@gmail.com>
2023-03-08 13:11:47 +0100

    Generación del grafo del pahfinding hecho
    He creado un algoritmo que se basa en recorrido en anchura con poda para generar el grafo que se usara para el pathfinding aún tengo que retocar un par de cosas pero parece que funciona

--- Assets/ScriptsAI/Pathfollowing/Grafo.cs
+++ Assets/ScriptsAI/Pathfollowing/Grafo.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 /*
  * Como se observa se le da un diccionario y el grafo se crea guardadno ese diccionario
+ * La he hecho MonoBehaviour con el fin de que se pueda dibujar el grafo y se pueda mostrar haciendo debug
  */
 public class Grafo : MonoBehaviour
 {
 
     private Dictionary<Vector3Int, List<Conexion>> grafo;
-   public Grafo(Dictionary<Vector3Int,List<Conexion>> contenidoGrafo)
-    {
-        grafo = contenidoGrafo;
-    }
+    private int L; //representa la longitud del lado del cuadrado del grid cyadrado
+    private int limite; //la profunidad escogida del grafo
+    private Vector3Int origenGeneracion; //es el punto correspondiente del grafo NO DEL MUNDO que es el origen desde el que se ha generado el grafo
+    private Vector3 origenMundo; //quiere decir la posicion original desde la que se llamo al metodo de generar el grafo
+    
+
     
     public List<Conexion> getConexiones(Vector3Int nodo)
     {
@@ -21,26 +24,54 @@ public class Grafo : MonoBehaviour
         
     }
 
+    /*
+     * Para acceder al diccionario y poder modificarlo o cambiar sus datos
+     */
+    public Dictionary<Vector3Int, List<Conexion>> Grafoo
+    {
+
+        get { return grafo; }
+    }
+
+
+    public void Start()
+    {
+
+        L = 1;
+        limite = 2;
+        origenMundo = transform.position; //La posicion del mundo
+        origenGeneracion = new Vector3Int(Mathf.RoundToInt(origenMundo.x), Mathf.RoundToInt(origenMundo.y), Mathf.RoundToInt(origenMundo.z));
+        grafo = GeneracionGrafoAnchura.generarGrafo(origenGeneracion, limite);
+        
+    }
 
     public void Update()
     {
-        //aqui no hace falta actualizar nada
+        //Debug.Log(grafo != null);
+        //if (grafo == null) grafo = GeneracionGrafoAnchura.generarGrafo(origenGeneracion, 3);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
-        
-        foreach( KeyValuePair<Vector3Int,List<Conexion>>  punto in grafo ) //se recorren las entradas par (clave,valor)
-        {
-            Gizmos.DrawSphere(punto.Key, 1); //parece que deja hacerlo con Vector3Int aun cuando acepta Vector3
 
-            Gizmos.color = Color.blue; //uso el color azul para las conexiones
-            foreach (Conexion c in punto.Value)
+       Debug.Log(grafo != null);
+        grafo = GeneracionGrafoAnchura.generarGrafo(Vector3Int.zero, 4);
+        if (grafo != null) //por si no hay grafo
+        {
+            
+            Gizmos.color = Color.yellow;
+
+            foreach (KeyValuePair<Vector3Int, List<Conexion>> punto in grafo) //se recorren las entradas par (clave,valor)
             {
-                Gizmos.DrawLine(c.Origen, c.Destino);
+                Gizmos.DrawSphere(punto.Key, 0.2f); //parece que deja hacerlo con Vector3Int aun cuando acepta Vector3
+
+                Gizmos.color = Color.blue; //uso el color azul para las conexiones
+                foreach (Conexion c in punto.Value)
+                {
+                    Gizmos.DrawLine(c.Origen, c.Destino);
+                }
+                Gizmos.color = Color.yellow; //para dibujar el siguiente nodo en amarillo
             }
-            Gizmos.color = Color.yellow; //para dibujar el siguiente nodo en amarillo
         }
     }
 
