3d9a4445fd375b0eb6ce557349b5ff51be60449a
DaniSanchez01 <danielsnchez01@gmail.com>
2023-04-21 12:23:46 +0200

    Movimiento de cámara Escenario
    WASD para moverse, flechas para rotar y moverse arriba y abajo

---
+++ Assets/ScriptsAI/Otros/cameraMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 0.4f;
+    float rotationSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = gameObject.transform.position;
+
+        if (Input.GetKey(KeyCode.W)) {
+            pos = pos + transform.forward*speed;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            pos = pos + transform.forward*-speed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            pos = pos + transform.right*-speed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            pos = pos + transform.right*speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            pos = new Vector3(pos.x,pos.y+ speed,pos.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            pos = new Vector3(pos.x,pos.y- speed,pos.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(0,-rotationSpeed,0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(0,rotationSpeed,0);
+        }
+
+        transform.position = pos;
+    }
+}
